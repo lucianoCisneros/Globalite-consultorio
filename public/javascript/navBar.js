2e72bee8e7380aeb501ff8c09163986d60840ee2
Luciano Cisneros <lucianocisneros22@gmail.com>
2021-01-31 18:44:45 -0300

    Se corrigio el margen de los parrafos, se agregaron detales visuales y se retocaron detalles de mobile

--- public/javascript/navBar.js
+++ public/javascript/navBar.js
@@ -1,8 +1,8 @@
 addEventListener('DOMContentLoaded', () => {
-    const btn_menu = document.querySelector('.menuBtn');
+    const btn_menu = document.querySelector('.menu__btn');
     if(btn_menu){
         btn_menu.addEventListener('click', () => {
-            const menu_items = document.querySelector('.menuItems')
+            const menu_items = document.querySelector('.menu__items')
             menu_items.classList.toggle('show')
         }
         )
