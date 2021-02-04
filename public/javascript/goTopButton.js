14264b37a1c0891a1bb2597cb3333989bb1d697c
Luciano Cisneros <lucianocisneros22@gmail.com>
2021-02-03 23:30:23 -0300

    Correcion de errores y agregado de animaciones

--- public/javascript/goTopButton.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = function (){
-    if (document.documentElement.scrollTop > 100){
-        document.querySelector('.go__top--container').classList.add('.show__button');
-    } else {
-        document.querySelector('.go__top--container').classList.remove('.show__button');
-    }
-}
